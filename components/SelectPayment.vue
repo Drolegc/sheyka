0ee4a4ff7e29f2466a0bf6dec8624dd0e9078bde
drolegc <drolegc@gmail.com>
2020-12-22 01:13:50 -0300

    Desarrollando pantalla de ordenes de compra

--- components/SelectPayment.vue
+++ components/SelectPayment.vue
@@ -1,6 +1,6 @@
 <template>
     <v-container >
-        <v-container class="d-flex justify-space-around align-center rounded-lg white" @click="">
+        <v-container class="d-flex justify-space-around align-center rounded-lg white" @click="dialog = true">
             <v-icon x-large>mdi-credit-card</v-icon>
         <span class="headline ml-3 text-center">Agregar metodo de pago</span>
         </v-container>
@@ -9,6 +9,7 @@
         v-model="dialog"
         transition="dialog-transition"
     >
+    
     </v-dialog>
     </v-container>
     
---
+++ pages/orders.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-container>
+        
+    </v-container>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                orders: []
+            }
+        },
+        created() {
+            this.$axios.get('/orders?user=' + this.$auth.user.id).then(response => this.orders = repsonse.data)
+        }
+    }
+</script>
