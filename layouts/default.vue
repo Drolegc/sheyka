0ad3109358b3a58b32dce50b7b0b18a64610a058
drolegc <drolegc@gmail.com>
2021-04-12 14:55:12 -0300

    Opcion en menu de terminos y condiciones y preguntas frecuentes para vovler al inicio

--- layouts/default.vue
+++ layouts/default.vue
@@ -16,6 +16,9 @@
         </v-btn>
       </template>
 <v-list v-if="$auth.loggedIn">
+    <v-list-item @click="$router.push('/')">
+        <v-list-item-title>Inicio</v-list-item-title>
+    </v-list-item>
     <v-list-item @click="$router.push('/profile/')">
         <v-list-item-title>Mi perfil</v-list-item-title>
     </v-list-item>
